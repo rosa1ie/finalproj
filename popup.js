c37970f6665fc725825a2bce1f40a6988766216c
Luka <luka.yep@gmail.com>
2023-12-01 14:59:05 -0500

    Facebook logout implemented

--- popup.js
+++ popup.js
@@ -1,68 +1,95 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var loginButton = document.getElementById('login-btn');
-    loginButton.addEventListener('click', function() {
-        loginWithFacebook();
-    });
-});
+    document.addEventListener('DOMContentLoaded', function() {
+        var loginButton = document.getElementById('login-btn');
 
-function loginWithFacebook() {
-    var clientId = '1097249097872208';
-    var redirectUri = 'https://ggehpmlgjdappfdakaefjcjmcdhleedk.chromiumapp.org/';
-
-    var authUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=email,user_likes`;
-
-    chrome.identity.launchWebAuthFlow({
-        url: authUrl,
-        interactive: true
-    }, function(redirectUrl) {
-        if (chrome.runtime.lastError) {
-            console.log(chrome.runtime.lastError.message);
-            return;
+        // Check if the user is already logged in
+        if (localStorage.getItem('accessToken')) {
+            fetchUserData(localStorage.getItem('accessToken'));
+            loginButton.textContent = 'Log Out';
         }
-        var accessToken = extractToken(redirectUrl);
-        fetchUserData(accessToken);
+
+        loginButton.addEventListener('click', function() {
+            if (loginButton.textContent === 'Log In') {
+                loginWithFacebook();
+            } else {
+                logout();
+            }
+        });
     });
-}
 
-function extractToken(url) {
-    var match = url.match(/#access_token=([^&]+)/);
-    return match ? match[1] : null;
-}
 
-function fetchUserData(token) {
-    var xhr = new XMLHttpRequest();
-    // Include 'likes' in the fields parameter
-    xhr.open('GET', 'https://graph.facebook.com/me?fields=id,name,email,likes.limit(1).summary(true)&access_token=' + token);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            var userData = JSON.parse(xhr.responseText);
-            displayUserData(userData);
-        } else {
-            console.error('Request failed.  Returned status of ' + xhr.status);
-        }
-    };
-    xhr.send();
-}
+    function loginWithFacebook() {
+        var clientId = '1097249097872208';
+        var redirectUri = 'https://ggehpmlgjdappfdakaefjcjmcdhleedk.chromiumapp.org/';
 
-function displayUserData(data) {
-    var displayArea = document.getElementById('user-data');
+        var authUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=email,user_likes&auth_type=reauthenticate`;
 
-    // Extracting user details
-    var userId = data.id;
-    var userName = data.name;
-    var userEmail = data.email;
-    var userLikesCount = data.likes.summary.total_count;
+        chrome.identity.launchWebAuthFlow({
+            url: authUrl,
+            interactive: true
+        }, function(redirectUrl) {
+            if (chrome.runtime.lastError) {
+                console.log(chrome.runtime.lastError.message);
+                return;
+            }
+            var accessToken = extractToken(redirectUrl);
+            localStorage.setItem('accessToken', accessToken);
+            fetchUserData(accessToken);
+        });
 
-    // Creating a neat display
-    var displayContent = `
-        <h3>User Information</h3>
-        <p><strong>ID:</strong> ${userId}</p>
-        <p><strong>Name:</strong> ${userName}</p>
-        <p><strong>Email:</strong> ${userEmail}</p>
-        <p><strong>Number of Likes:</strong> ${userLikesCount}</p>
-    `;
+        var loginButton = document.getElementById('login-btn');
+        loginButton.textContent = 'Log Out';
+    }
 
-    // Setting the innerHTML of the display area
-    displayArea.innerHTML = displayContent;
-}
+    function logout() {
+        // Clear user data and access token
+        localStorage.removeItem('accessToken');
+        document.getElementById('user-data').innerHTML = '';
+        document.getElementById('login-btn').textContent = 'Log In';
+    
+        // Redirect to Facebook logout URL
+        var logoutUrl = `https://www.facebook.com/logout.php?next=${encodeURIComponent(window.location.href)}&access_token=${localStorage.getItem('accessToken')}`;
+        window.location.href = logoutUrl;
+    }
+    
+    function extractToken(url) {
+        var match = url.match(/#access_token=([^&]+)/);
+        return match ? match[1] : null;
+    }
+
+    function fetchUserData(token) {
+        var xhr = new XMLHttpRequest();
+        // Include 'likes' in the fields parameter
+        xhr.open('GET', 'https://graph.facebook.com/me?fields=id,name,email,likes.limit(1).summary(true)&access_token=' + token);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                var userData = JSON.parse(xhr.responseText);
+                displayUserData(userData);
+            } else {
+                console.error('Request failed.  Returned status of ' + xhr.status);
+            }
+        };
+        xhr.send();
+    }
+
+    function displayUserData(data) {
+        var displayArea = document.getElementById('user-data');
+
+        // Extracting user details
+        var userId = data.id;
+        var userName = data.name;
+        var userEmail = data.email;
+        var userLikesCount = data.likes.summary.total_count;
+
+        // Creating a neat display
+        var displayContent = `
+            <h3>Information Collected by Meta</h3>
+            <p><strong>ID:</strong> ${userId}</p>
+            <p><strong>Name:</strong> ${userName}</p>
+            <p><strong>Email:</strong> ${userEmail}</p>
+            <p><strong>Number of Likes:</strong> ${userLikesCount}</p>
+        `;
+
+        // Setting the innerHTML of the display area
+        displayArea.innerHTML = displayContent;
+    }
 
